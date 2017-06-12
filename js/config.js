9e13c959c09a23cc6d954efce24f511a865869fe
user <priyanka.vantage123@gmail.com>
2017-06-12 12:44:26 +0530

    Initaila updates

---
+++ js/config.js
@@ -0,0 +1,22 @@
+var siteurl = "http://localhost/event_pp/index.php";
+
+function gup(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
+
+
+function gotoBack(){
+	history.go(-1);
+    navigator.app.backHistory();	
+}
