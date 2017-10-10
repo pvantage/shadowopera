5cc414f8ce5a53041867223a4ca9af18c0fb2d29
user <priyanka.vantage123@gmail.com>
2017-10-10 11:09:18 +0530

    updates

--- js/config.js
+++ js/config.js
@@ -1,15 +1,15 @@
-var siteurl = "http://shadowopera.vantagewebtech.com/index.php";
+var siteurl = "http://shadowopera.com/app/index.php";
 
-function gup(sParam) {
+function gup(sParam){
   var sPageURL = decodeURIComponent(window.location.search.substring(1)),
         sURLVariables = sPageURL.split('&'),
         sParameterName,
         i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
+    for(i = 0; i < sURLVariables.length; i++){
         sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
+        if(sParameterName[0] === sParam){
             return sParameterName[1] === undefined ? true : sParameterName[1];
         }
     }
@@ -17,30 +17,28 @@ function gup(sParam) {
 
 function gotoBack(){
 	history.go(-1);
-    navigator.app.backHistory();	
+    navigator.app.backHistory();
 }
 
 window.addEventListener("orientationchange", function(){
-   
+
 });
+
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady(){
-	
+
 	localStorage.setItem('deviceplatform', device.platform);
 	var path = window.location.pathname;
 	var page = path.split("/").pop();
 
-	
-	if(page=='live.html'){
-		screen.orientation.lock('landscape');
+	if(page=='live.html' || page=='play.html' || page=='playvideo.html'){
+		//screen.orientation.lock('landscape');
 		screen.orientation.lock('landscape-primary');
 		
 	}
 	else
 	{
-		screen.orientation.lock('portrait');
+		//screen.orientation.lock('portrait');
 		screen.orientation.lock('portrait-primary');
-	}
-	
+	}	
 };
-
