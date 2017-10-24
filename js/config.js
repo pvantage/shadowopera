800949cd2bdd83c0e01e417bbfb5a88148a82ae0
user <priyanka.vantage123@gmail.com>
2017-10-24 17:43:08 +0530

    updates

--- js/config.js
+++ js/config.js
@@ -1,15 +1,15 @@
 var siteurl = "http://shadowopera.com/app/index.php";
 
-function gup(sParam){
+function gup(sParam) {
   var sPageURL = decodeURIComponent(window.location.search.substring(1)),
         sURLVariables = sPageURL.split('&'),
         sParameterName,
         i;
 
-    for(i = 0; i < sURLVariables.length; i++){
+    for (i = 0; i < sURLVariables.length; i++) {
         sParameterName = sURLVariables[i].split('=');
 
-        if(sParameterName[0] === sParam){
+        if (sParameterName[0] === sParam) {
             return sParameterName[1] === undefined ? true : sParameterName[1];
         }
     }
@@ -17,16 +17,16 @@ function gup(sParam){
 
 function gotoBack(){
 	history.go(-1);
-    navigator.app.backHistory();
+    navigator.app.backHistory();	
 }
 
 window.addEventListener("orientationchange", function(){
-
+   
 });
 
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady(){
-
+	
 	localStorage.setItem('deviceplatform', device.platform);
 	var path = window.location.pathname;
 	var page = path.split("/").pop();
@@ -40,5 +40,7 @@ function onDeviceReady(){
 	{
 		//screen.orientation.lock('portrait');
 		screen.orientation.lock('portrait-primary');
-	}	
+	}
+	
 };
+
